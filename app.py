fcd7bb75ce971179ffd4b3e9299b9af55bf225d8
Armel KI <kiarmelstephanenovak@gmail.com>
2026-02-06 01:09:25 +0100

    Ajout d'une route pour ajouter une résolution à partir d'un formulaire

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, url_for, redirect, request
+from datetime import date
+import random
 
 resolutions = [
 {
@@ -36,6 +38,27 @@ def status():
 @app.route('/')
 def index():
     return render_template('index.html', resolutions=resolutions)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    message = None
+    if request.method == 'POST':
+        pseudo = request.form.get('pseudo')
+        texte = request.form.get('texte')
+        new_id = len(resolutions) + 1
+        new_res = {
+            "id": new_id,
+            "pseudo": pseudo,
+            "texte": texte,
+            "votes": 0,
+            "statut": "active",
+            "created_at": date.today().isoformat()
+        }
+        resolutions.append(new_res)
+        messages_succes= ["Courage, on y croit !","Bonne chance !", "Go !"]
+        message = random.choice(messages_succes)
+        
+    return render_template('add.html', message=message)
     
 if __name__== '__main__':
     app.run(debug=True, host='0.0.0.0', port=5454)
